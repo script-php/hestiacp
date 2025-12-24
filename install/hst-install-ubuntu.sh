c0b5a6dee1634916c6be442d7f1bb5babaf4f3ee
YoYo <l.voinea@yahoo.com>
2025-12-24 21:00:58 +0200

    .

--- install/hst-install-ubuntu.sh
+++ install/hst-install-ubuntu.sh
@@ -1202,23 +1202,23 @@ echo "========================================================================"
 echo
 
 # Install Hestia packages from local folder or download from repository
-# if [ -n "$withdebs" ] && [ -d "$withdebs" ]; then
-# 	echo "[ * ] Installing Hestia packages from local folder..."
-# 	echo "    - hestia core package"
-# 	dpkg -i $withdebs/hestia_*.deb > /dev/null 2>&1
-# 	check_result $? "Failed to install hestia package from local folder"
+if [ -n "$withdebs" ] && [ -d "$withdebs" ]; then
+	echo "[ * ] Installing Hestia packages from local folder..."
+	echo "    - hestia core package"
+	dpkg -i $withdebs/hestia_*.deb > /dev/null 2>&1
+	check_result $? "Failed to install hestia package from local folder"
 
-# 	echo "    - hestia-php backend package"
-# 	dpkg -i $withdebs/hestia-php_*.deb > /dev/null 2>&1
-# 	check_result $? "Failed to install hestia-php package from local folder"
+	echo "    - hestia-php backend package"
+	dpkg -i $withdebs/hestia-php_*.deb > /dev/null 2>&1
+	check_result $? "Failed to install hestia-php package from local folder"
 
-# 	echo "    - hestia-nginx backend package"
-# 	dpkg -i $withdebs/hestia-nginx_*.deb > /dev/null 2>&1
-# 	check_result $? "Failed to install hestia-nginx package from local folder"
-# else
-# 	# Download and install Hestia packages from repository
-# 	install_hestia_packages
-# fi
+	echo "    - hestia-nginx backend package"
+	dpkg -i $withdebs/hestia-nginx_*.deb > /dev/null 2>&1
+	check_result $? "Failed to install hestia-nginx package from local folder"
+else
+	# Download and install Hestia packages from repository
+	install_hestia_packages
+fi
 
 # Restoring autostart policy
 rm -f /usr/sbin/policy-rc.d

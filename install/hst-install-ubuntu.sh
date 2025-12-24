c516e7c74d36cca97664c06dd65603315f3e915e
YoYo <l.voinea@yahoo.com>
2025-12-24 20:56:52 +0200

    .

--- install/hst-install-ubuntu.sh
+++ install/hst-install-ubuntu.sh
@@ -126,7 +126,6 @@ install_hestia_packages() {
 
 	echo "    - Installing hestia package"
 	dpkg -i "${temp_dir}/hestia_${HESTIA_INSTALL_VER}_amd64.deb" >> $LOG 2>&1
-	apt-get install -f -y >> $LOG 2>&1
 	check_result $? "Failed to install hestia package"
 
 	# Download hestia-php package
@@ -139,13 +138,7 @@ install_hestia_packages() {
 
 	echo "    - Installing hestia-php package"
 	dpkg -i "${temp_dir}/hestia-php_${HESTIA_PHP_VER}_amd64.deb" >> $LOG 2>&1
-	apt-get install -f -y >> $LOG 2>&1
-	if [ $? -ne 0 ]; then
-		echo "      ERROR: Failed to install hestia-php package"
-		echo "      Run: apt-get install -f to fix dependencies"
-		tail -20 $LOG
-		check_result 1 "Installation failed - check log above"
-	fi
+	check_result $? "Failed to install hestia-php package"
 
 	# Download hestia-nginx package
 	echo "    - Downloading hestia-nginx_${HESTIA_NGINX_VER}_amd64.deb..."
@@ -157,7 +150,6 @@ install_hestia_packages() {
 
 	echo "    - Installing hestia-nginx package"
 	dpkg -i "${temp_dir}/hestia-nginx_${HESTIA_NGINX_VER}_amd64.deb" >> $LOG 2>&1
-	apt-get install -f -y >> $LOG 2>&1
 	if [ $? -ne 0 ]; then
 		echo "      ERROR: Failed to install hestia-nginx package"
 		echo "      Run: apt-get install -f to fix dependencies"
